4a65f6bf76199dacde721497ec9d8dd34881934b
gcoron <germanezequielcoronel@gmail.com>
2020-09-02 18:32:32 -0300

    Initial commit

---
+++ app/src/main/java/com/dondeestanapp/DriverActivity.java
@@ -0,0 +1,16 @@
+package com.dondeestanapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.dondeestanapp.R;
+
+public class DriverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.driver_activity);
+    }
+}
---
+++ app/src/main/java/com/dondeestanapp/UserTypeActivity.java
@@ -0,0 +1,16 @@
+package com.dondeestanapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.dondeestanapp.R;
+
+public class UserTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_type_activity);
+    }
+}
