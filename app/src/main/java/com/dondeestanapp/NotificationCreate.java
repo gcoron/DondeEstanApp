172257cb8b174ff68c9502b30c3a7713c3513640
gcoron <33733200+gcoron@users.noreply.github.com>
2020-09-04 09:40:12 -0300

    Mockup layout (#1)
    * Layout mockup to driver creation
    
    * layout-mockup-stage2

---
+++ app/src/main/java/com/dondeestanapp/NotificationCreate.java
@@ -0,0 +1,16 @@
+package com.dondeestanapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.dondeestanapp.R;
+
+public class NotificationCreate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification_create);
+    }
+}
